8c826712560a8a79d676253a8c1c87106668ddaa
Hiroshi Kori <hiroshi.maybe@gmail.com>
2024-12-23 15:02:30 -0800

    solve 2024 Day 13 Part 2

---
+++ 2024/advent-of-code-2024/day13part2.rs
@@ -0,0 +1,172 @@
+#![allow(unused_macros, unused_imports)]
+use std::cmp::*;
+use std::collections::*;
+
+// $ cp-batch day13part2 | diff day13part2.out -
+// $ cargo run --bin day13part2
+
+///
+/// 12/23/2024
+///
+/// 14:39-15:02 AC
+///
+
+fn solve() -> i64 {
+    // let n = 15;
+    let n = 1279;
+
+    let mut res = 0;
+    let mut x1 = None;
+    let mut y1 = None;
+    let mut x2 = None;
+    let mut y2 = None;
+    for _ in 0..n {
+        let s = readln_str();
+        let s = s.trim_end();
+        if s.starts_with("Button A: X+") {
+            let s = s.trim_start_matches("Button A: X+");
+            let ds = s.split(", Y+").collect_vec();
+            // dbgln!(ds);
+            (x1, y1) = (
+                Some(ds[0].parse::<i64>().unwrap()),
+                Some(ds[1].parse::<i64>().unwrap()),
+            );
+            continue;
+        }
+
+        if s.starts_with("Button B: X+") {
+            let s = s.trim_start_matches("Button B: X+");
+            let ds = s.split(", Y+").collect_vec();
+            // dbgln!(ds);
+            (x2, y2) = (
+                Some(ds[0].parse::<i64>().unwrap()),
+                Some(ds[1].parse::<i64>().unwrap()),
+            );
+            continue;
+        }
+
+        if s.starts_with("Prize: X=") {
+            let s = s.trim_start_matches("Prize: X=");
+            let ds = s.split(", Y=").collect_vec();
+            // dbgln!(ds);
+            let (dx, dy) = (
+                Some(ds[0].parse::<i64>().unwrap()),
+                Some(ds[1].parse::<i64>().unwrap()),
+            );
+
+            res += f(
+                x1.unwrap(),
+                y1.unwrap(),
+                x2.unwrap(),
+                y2.unwrap(),
+                dx.unwrap() + 10000000000000,
+                dy.unwrap() + 10000000000000,
+            );
+        }
+    }
+
+    res
+}
+
+fn f(x1: i64, y1: i64, x2: i64, y2: i64, dx: i64, dy: i64) -> i64 {
+    // x1 * i + x2 * j = dx
+    // y1 * i + y2 * j = dy
+
+    let a = dx * y1 - dy * x1;
+    let b = x2 * y1 - x1 * y2;
+    if a % b != 0 {
+        return 0;
+    }
+    let j = a / b;
+
+    let a = dy * x2 - dx * y2;
+    let b = x2 * y1 - x1 * y2;
+    if a % b != 0 {
+        return 0;
+    }
+    let i = a / b;
+
+    3 * i + j
+}
+
+fn main() {
+    setup_out!(put, puts);
+
+    let res = solve();
+    puts!("{}", res);
+}
+
+use crate::cplib::io::*;
+use crate::cplib::minmax::*;
+use crate::cplib::vec::*;
+// region: cplib
+#[rustfmt::skip]
+#[allow(dead_code)]
+pub mod cplib {
+pub mod io {
+	macro_rules! _with_dollar_sign { ($($body:tt)*) => { macro_rules! __with_dollar_sign { $($body)* } __with_dollar_sign!($); }}
+	macro_rules! setup_out {
+		($fn:ident,$fn_s:ident) => {
+			use std::io::Write;
+			let out = std::io::stdout();
+			let mut out = ::std::io::BufWriter::new(out.lock());
+				_with_dollar_sign! { ($d:tt) => {
+				macro_rules! $fn { ($d($format:tt)*) => { let _ = write!(out,$d($format)*); } }
+				macro_rules! $fn_s { ($d($format:tt)*) => { let _ = writeln!(out,$d($format)*); } }
+			}}
+		};
+	}
+	macro_rules! _epr { ($v:expr $(,)?) => {eprint!("{} = {:?}, ", stringify!($v), $v)}; }
+	macro_rules! dbgln { ($($val:expr),*) => {{ eprint!("[{}:{}] ", file!(), line!()); ($(_epr!($val)),*); eprintln!(); }}; }
+	pub fn readln_str() -> String {
+		let mut line = String::new();
+		::std::io::stdin().read_line(&mut line).unwrap_or_else(|e| panic!("{}", e));
+		line
+	}
+	macro_rules! _read {
+		($it:ident; [char]) => { _read!($it; String).chars().collect::<Vec<_>>() };
+		($it:ident; [u8]) => { Vec::from(_read!($it; String).into_bytes()) };
+		($it:ident; usize1) => { $it.next().unwrap_or_else(|| panic!("input mismatch")).parse::<usize>().unwrap_or_else(|e| panic!("{}", e)) - 1 };
+		($it:ident; [usize1]) => { $it.map(|s| s.parse::<usize>().unwrap_or_else(|e| panic!("{}", e)) - 1).collect::<Vec<_>>() };
+		($it:ident; [$t:ty]) => { $it.map(|s| s.parse::<$t>().unwrap_or_else(|e| panic!("{}", e))).collect::<Vec<_>>() };
+		($it:ident; $t:ty) => { $it.next().unwrap_or_else(|| panic!("input mismatch")).parse::<$t>().unwrap_or_else(|e| panic!("{}", e)) };
+		($it:ident; $($t:tt),+) => { ($(_read!($it; $t)),*) };
+	}
+	macro_rules! readlns {
+		($($t:tt),*; $n:expr) => {{ let stdin = ::std::io::stdin();
+			::std::io::BufRead::lines(stdin.lock()).take($n).map(|line| {
+				let line = line.unwrap(); #[allow(unused_mut)]let mut it = line.split_whitespace(); _read!(it; $($t),*)
+			}).collect::<Vec<_>>()
+		}};
+	}
+	macro_rules! readln {
+		($($t:tt),*) => {{ let line = cplib::io::readln_str(); #[allow(unused_mut)]let mut it = line.split_whitespace(); _read!(it; $($t),*) }};
+	}
+	pub(crate) use {readlns, readln, setup_out, dbgln, _with_dollar_sign, _epr, _read};
+}
+pub mod minmax {
+	pub trait SetMinMax {
+		fn setmin(&mut self, other: Self) -> bool;
+		fn setmax(&mut self, other: Self) -> bool;
+	}
+	macro_rules! _set { ($self:ident, $cmp:tt, $other:ident) => {{
+			let update = $other $cmp *$self;
+			if update { *$self = $other; }
+			update
+	}}; }
+	impl<T> SetMinMax for T where T: PartialOrd {
+		fn setmin(&mut self, other: T) -> bool { _set!(self, <, other) }
+		fn setmax(&mut self, other: T) -> bool { _set!(self, >, other) }
+	}
+}
+pub mod vec {
+	pub trait CollectVec: Iterator { fn collect_vec(self) -> Vec<Self::Item> where Self: Sized { self.collect() } }
+	impl<T> CollectVec for T where T: Iterator {}
+	macro_rules! vvec {
+		($v:expr; $n:expr) => { Vec::from(vec![$v; $n]) };
+		($v:expr; $n:expr $(; $ns:expr)+) => { Vec::from(vec![vvec![$v $(; $ns)*]; $n]) };
+	}
+	pub(crate) use vvec;
+}
+}
+// endregion: cplib
